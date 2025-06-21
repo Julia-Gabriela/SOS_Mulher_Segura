bb3d816fbd73e06c7386354f27760e62780c1be1
Julia Gabriela <juliagabrielagomes09@gmail.com>
2025-06-21 14:51:05 -0300

    Criação das páginas Sobre e Contato com layout 2 colunas + ajustes de URLs e estrutura

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Windows 11\Pictures\SOS_Mulher_Segura\venv"
+set "VIRTUAL_ENV=D:\Faculdade\4_semestre\SOS_Mulher_Segura\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
